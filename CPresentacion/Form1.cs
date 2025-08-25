1d8fbf48f925b1c5bfe9ea9a77f1d00909db5e6b
sturtola <turtolasabrina@gmail.com>
2025-08-25 16:35:38 -0300

    Comenzando con el diseño de la pantalla de inicio del sistema

--- CPresentacion/Form1.cs
+++ CPresentacion/Form1.cs
@@ -1,8 +1,8 @@
 namespace AurenPadelStore
 {
-    public partial class Form1 : Form
+    public partial class FIniciarSesion : Form
     {
-        public Form1()
+        public FIniciarSesion()
         {
             InitializeComponent();
         }
